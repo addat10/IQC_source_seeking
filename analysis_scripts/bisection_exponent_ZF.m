d1174427bce7d24ef6e05728f76e9a0adb32bade
Adwait <datar.adwait@gmail.com>
2021-06-29 23:48:00 +0200

    Add first Naive implementation of ZF multipliers

---
+++ analysis_scripts/bisection_exponent_ZF.m
@@ -0,0 +1,31 @@
+function [alpha_best,P]=bisection_exponent_ZF(G_veh,m,L,dim,alpha_lims,cvx_tol,bisect_tol)
+% This function runs a bisection algorithm for obtaining the best
+% exponent alpha by calling the analysis routine for a fixed alpha
+% multiple times.
+    
+    % Analyze for alpha=alpha_low    
+    %[status,P]=verify_exp_stab_FBCC(Psi_G_I,alpha_lims(1),m,L,dim,cvx_tol);
+    [status,P]=verify_exp_stab_ZF(G_veh,alpha_lims(1),m,L,dim,cvx_tol);
+    if ~status
+        error('Infeasible for alpha_low. Choose a smaller alpha_low')
+    end
+    
+    % Analyze for alpha=alpha_high    
+    %[status,P]=verify_exp_stab_FBCC(Psi_G_I,alpha_lims(2),m,L,dim,cvx_tol);
+    [status,P]=verify_exp_stab_ZF(G_veh,alpha_lims(2),m,L,dim,cvx_tol);
+    if status        
+        alpha_best=alpha_lims(2); % Return alpha_high if feasible
+    else
+        while alpha_lims(2)-alpha_lims(1)>bisect_tol
+            alpha_mid=mean(alpha_lims);           
+            %[status,P]=verify_exp_stab_FBCC(Psi_G_I,alpha_mid,m,L,dim,cvx_tol);
+            [status,P]=verify_exp_stab_ZF(G_veh,alpha_mid,m,L,dim,cvx_tol);
+            if status
+                alpha_lims(1)=alpha_mid;
+            else
+                alpha_lims(2)=alpha_mid;        
+            end
+        end
+        alpha_best=alpha_lims(1);
+    end
+end
